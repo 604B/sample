ba8835edbf4cd5379cf2c8ad1541ec273f91b7e0
604B <moritaka_1971@wdc-kd.com>
2026-07-02 14:53:49 +0900

    update 2026/07/02

---
+++ carousel/js/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".swiper", {
+    // 基本設定
+    direction: "horizontal", // 水平スライダー
+    loop: true, // ループ有効化
+    autoplay: {
+        delay: 3000, // 自動再生の間隔
+    },
+    // ページネーション
+    pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+        clickable: true, // ページネーションのクリック有効化
+    },
+    // ナビゲーション
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // アニメーション
+    effect: "cube",
+});
